83e0b5bb87dbc46cc2c9bcd1a8cdbc947058b452
Ivan Marzan <87321166+MarzanIvan@users.noreply.github.com>
2023-01-21 13:31:24 +0300

    Handler of ISR USART_rx was changed to check buffer full in the body of the function LockBuffer

--- src/Config_Interrupts.cpp
+++ src/Config_Interrupts.cpp
@@ -5,8 +5,7 @@ Stream_Buffer Buffer;
 
 ISR(USART_RX_vect) {
     char Descriptor = 0x01;
-    if (Buffer.StateOfBuffer == FREE) {
-        Buffer.LockBuffer(Descriptor);
+    if (Buffer.LockBuffer(Descriptor)) {
         Buffer.Push(UDR0);
         Buffer.UnlockBuffer(Descriptor);
     }
