87897652bd98b2bc0a35cdd35e259f113d846e2b
Ivan Marzan <87321166+MarzanIvan@users.noreply.github.com>
2023-01-21 13:40:39 +0300

    Worthless src/Buffer.cpp source code file was deleted because functions of struct Stream_Buffer must be inline to make program more fast

--- include/Buffer.h
+++ include/Buffer.h
@@ -24,15 +24,50 @@ struct Stream_Buffer {
         
     }
 
-    inline bool Push(char Letter);
+inline bool Stream_Buffer::Push(char Letter) {
+    if (IndexOfNextElement < SizeOfBuffer) {
+        Stack[IndexOfNextElement++] = Letter;
+        Stack[IndexOfNextElement] = '\0';
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
-    inline char *PullAll();
+inline char *Stream_Buffer::PullAll() {
+    if (IndexOfNextElement > 0) {
+        return Stack;
+    }
+    else {
+        return '\0';
+    }
+}
 
-    inline bool UnlockBuffer(char UnlockerDescriptor);
+inline bool Stream_Buffer::UnlockBuffer(char UnlockerDescriptor) {
+    if (UnlockerDescriptor != Descriptor) {
+        return false;
+    }
+    StateOfBuffer = FREE; 
+    return true;
+}
 
-    inline bool LockBuffer(char LockerDescriptor);
+inline bool Stream_Buffer::LockBuffer(char LockerDescriptor) {
+    if (StateOfBuffer != FREE) {
+        return false;
+    }
+    Descriptor = LockerDescriptor; 
+    StateOfBuffer = LOCKED;
+    return true;
+}
 
-    inline bool GetResponceToUseBuffer( char DescriptorOfUser);
+inline bool Stream_Buffer::GetResponceToUseBuffer( char DescriptorOfUser) {
+    if (DescriptorOfUser == Descriptor) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 };
 
