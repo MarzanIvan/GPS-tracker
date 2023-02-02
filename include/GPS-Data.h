f6fffdbe23b663ff1d9c9b096910ab0e1e4b2394
Ivan Marzan <87321166+MarzanIvan@users.noreply.github.com>
2023-02-02 23:49:12 +0300

    Function to Check having of a substring in a string

--- include/GPS-Data.h
+++ include/GPS-Data.h
@@ -2,7 +2,13 @@
     #define GPSDATAH
     
 struct GPSData {
-    
+    double Latitude;
+    double Longitude;
+    double InitialLatitude;
+    double InitialLongitude;
+
 };
+
+bool DoesStringContainSubString( const char *Array, const char* SubString);
 
 #endif
--- src/GPS-Data.cpp
+++ src/GPS-Data.cpp
@@ -1,1 +1,18 @@
 #include "../include/GPS-Data.h"
+
+bool DoesStringContainSubString( const char *Array, const char* SubString) {
+	while (*Array) {
+		if (*Array == (*SubString)) {
+			const char *TempStr = Array;
+			while (*TempStr == *SubString) {
+				if (*TempStr != *SubString) {
+					return false;
+				}
+				TempStr++;
+				SubString++;
+			}
+			return true;
+		}
+		Array++;
+	}
+}
